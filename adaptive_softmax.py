192da5bc2f0a24e29d247048eb48ab2376525823
Frontman <isong.nathan90@gmail.com>
2025-02-19 23:49:20 +0100

    Update adaptive_softmax.py

--- adaptive_softmax.py
+++ adaptive_softmax.py
@@ -31,18 +31,17 @@ class AdaptiveSoftmax(tf.keras.layers.Layer):
         self.compute_proj_dims(hidden_dim)
 
         head_dim = self.cutoffs[0] + self.cluster_num
-        self.head_w = tf.keras.layers.Dense(head_dim, name='ada_softmax_head_w', input_shape=(hidden_dim,))
+        self.head_w = tf.keras.layers.Dense(head_dim, name='ada_softmax_head_w', input_shape=[None, hidden_dim])
 
         self.tail_w = []
         for i in range(self.cluster_num):
             tail_dim = self.cutoffs[i + 1] - self.cutoffs[i]
             self.tail_w.append(
-                tf.keras.layers.TimeDistributed(tf.keras.models.Sequential([
-                    tf.keras.layers.Dense(self.proj_dims[i], name=f"ada_softmax_tail{i+1}_proj", input_shape=(hidden_dim,)),
+                tf.keras.models.Sequential([
+                    tf.keras.layers.Dense(self.proj_dims[i], name=f"ada_softmax_tail{i+1}_proj", input_shape=[None, hidden_dim]),
                     tf.keras.layers.Dense(tail_dim, name=f"ada_softmax_tail{i+1}_w")
                 ])
               )
-            )
         super().build(input_shape)
 
     def _loss(self, inp, labels, reduction='auto'):
