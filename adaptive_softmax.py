6f2251877e89752257fdf3da1c2775448eccaf29
Frontman <isong.nathan90@gmail.com>
2025-02-20 01:11:38 +0100

    Update adaptive_softmax.py
    Added support for masking.

--- adaptive_softmax.py
+++ adaptive_softmax.py
@@ -118,12 +118,13 @@ class AdaptiveSoftmax(tf.keras.layers.Layer):
             output = tf.concat([output, tail_softmax], axis=-1)
         return output
 
-    def call(self, inp, labels=None, use_log=False, reduction='auto'):
+    def call(self, inp, labels=None, use_log=False, reduction='auto', mask=None):
         if labels is None:
             return self._log_softmax(inp) if use_log else self._softmax(inp)
         return self._loss(inp, labels, reduction)
 
-
+    def compute_mask(self, input, mask=None):
+        return mask
 
 
 
