f0d9bc8233a3f4104be7f20faa519b40af30030e
Frontman <isong.nathan90@gmail.com>
2025-03-01 15:32:53 +0100

    Update adaptive_softmax.py

--- adaptive_softmax.py
+++ adaptive_softmax.py
@@ -177,10 +177,10 @@ class AdaptiveSoftmax(tf.keras.layers.Layer):
             output = tf.concat([output, tail_softmax], axis=-1)
         return output
 
-    def call(self, inp, labels=None, use_log=False, reduction='auto', mask=None):
+    def call(self, labels=None, inp=None, use_log=False, reduction='auto', mask=None):
         if labels is None:
             return self._log_softmax(inp) if use_log else self._softmax(inp)
-        return self._loss(inp, labels, reduction)
+        return self._loss(labels, inp, reduction)
 
     def compute_mask(self, input, mask=None):
         # print(mask.shape, mask)
