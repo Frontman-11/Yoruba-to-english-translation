a3b2cd919d674e7551aed8d2834ca9be469efb0c
Frontman <isong.nathan90@gmail.com>
2025-03-01 09:46:50 +0100

    Update tokenizer_function.py

--- tokenizer_function.py
+++ tokenizer_function.py
@@ -11,8 +11,8 @@ class FrontmanTokenizer(spm.SentencePieceProcessor):
     def encode(self, text, out_type='tf', exclude_token_ids=None, with_attention_mask=True, **kwargs):
         # Ensure text is a regular Python string, not a Tensor
         if isinstance(text, tf.Tensor):
-            text = text.tolist()
-            text = [txt.numpy().decode("utf-8") for txt in text]  # Convert Tensor -> NumPy -> String
+            text = text.numpy().tolist()
+            text = [txt.decode("utf-8") for txt in text]  # Convert Tensor -> NumPy -> String
     
         if out_type == 'tf':
             input_ids = super().encode(text, out_type=int, **kwargs)  # Call SentencePiece encode
