b14c0c3969f585951b96f5f8be7ef9df76ce3df2
Frontman <isong.nathan90@gmail.com>
2025-05-21 00:26:22 +0100

    Update tokenizer_function.py

--- tokenizer_function.py
+++ tokenizer_function.py
@@ -60,4 +60,5 @@ class FrontmanTokenizer(spm.SentencePieceProcessor):
     def decode(self, input_ids, out_type=str, **kwargs):
         if isinstance(input_ids, tf.Tensor):
             input_ids = input_ids.numpy().tolist()
+            print(input_ids)
         return super().decode(input_ids, out_type=out_type, **kwargs)
